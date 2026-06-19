af095e495c65691e1c3493d5030b9bd1c23b2514
Faustino <sachielcristo@gmail.com>
2026-06-19 13:05:47 -0600

    feat(sideBar):se agrego la funcion de un sidebar dinamico

--- src/components/layout/navBar.vue
+++ src/components/layout/navBar.vue
@@ -3,7 +3,7 @@
     <nav class="flex items-center px-4 py-3 gap-4">
       
       <!-- Botón hamburguesa -->
-      <button class="hover:text-pink-700">
+      <button @click="toggleMenu" class="hover:text-pink-700">
         <Menu :size="24" />
       </button>
 
@@ -29,7 +29,7 @@
         <RouterLink to="/login" class="hover:text-pink-700">
             <User :size="24" />
         </RouterLink>
-        <RouterLink to="/catalogo/carrito" class="hover:text-pink-700">
+        <RouterLink to="/catalago/carrito" class="hover:text-pink-700">
             <ShoppingBag :size="24"/>
         </RouterLink>
       </div>
@@ -41,5 +41,12 @@
 <script setup>
 import { RouterLink } from 'vue-router';
 import { Menu, Search, User, ShoppingBag } from '@lucide/vue'
+import { inject } from 'vue';
+
+const isSidebarOpen = inject('isSidebarOpen')
+
+function toggleMenu() {
+  isSidebarOpen.value = !isSidebarOpen.value
+}
 
 </script>
