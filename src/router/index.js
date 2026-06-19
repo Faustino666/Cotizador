b2999bdd5d8d7dc6e51a6170eb5e36f87bacd9f9
Faustino <sachielcristo@gmail.com>
2026-06-19 12:03:53 -0600

    refactor(index.js): -se agreagaron rutas

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,8 @@ const routes = [
   { path: '/catalogo',      name: 'catalogo', component: () => import('../views/Catalogo.vue') },
   { path: '/producto/:id',  name: 'producto', component: () => import('../views/detalleProducto.vue') },
   { path: '/login',         name: 'login',    component: () => import('../views/login.vue') },
-  { path: '/register',      name: 'register', component: () => import('../views/Registrer.vue') }
+  { path: '/register',      name: 'register', component: () => import('../views/Registrer.vue') },
+  { path: '/catalago/carrito', name: 'catalago-carrito', component: () => import('../views/carrito.vue')}
 ]
 
 const router = createRouter({
